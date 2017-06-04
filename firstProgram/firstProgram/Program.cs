f6322a2b0693d433ef13fe1969d548883872101d
ahmedshukry <ahmedshukry84@gmail.com>
2017-06-05 00:10:14 +0200

    First Csharp Program
    This is my first CSharp Progra,

---
+++ firstProgram/firstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace firstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This Is First Program
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
