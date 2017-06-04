d8d6d2be5470359c94f99cdce4999e3532e91126
ahmedshukry <ahmedshukry84@gmail.com>
2017-06-05 00:45:05 +0200

    first variable
    this is my first variable program

---
+++ firstVariable/firstVariable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace firstVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("My Value Is: {0}", x);
+            Console.ReadKey();
+        }
+    }
+}
